fdd79355daef6ab11cbf1b726c8a1a4598f463f6
nathanGouacheg22017449 <nathan.gouache@etu.univ-amu.fr>
2024-04-02 10:01:48 +0200

    marche pas

--- src/main/java/fr/univamu/iut/plats/GetPlats.java
+++ src/main/java/fr/univamu/iut/plats/GetPlats.java
@@ -1,9 +1,13 @@
 package fr.univamu.iut.plats;
 
 import jakarta.ws.rs.*;
-import fr.univamu.iut.SqlRequests;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import fr.univamu.iut.SqlRequests;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 @Path("/plat")
 public class GetPlats {
@@ -12,6 +16,23 @@ public class GetPlats {
     @Produces("text/plain")
     @Path("/getIdByName/{name}")
     public String getIdByName(@PathParam("name") String name) throws SQLException, ClassNotFoundException {
-        return SqlRequests.executeQuery("SELECT id FROM Plats WHERE nom='"+name+"'").toString();
+        ResultSet resultSet = SqlRequests.executeQuery("SELECT id FROM Plats WHERE nom='" + name + "'");
+        JSONArray result = new JSONArray();
+
+        try {
+            while (resultSet.next()) {
+                JSONObject row = new JSONObject();
+                // Récupérer la valeur de la colonne "id"
+                int id = resultSet.getInt("id");
+                // Stocker la valeur dans le JSONObject
+                row.put("Id", id);
+                // Ajouter l'objet à la JSONArray
+                result.put(row);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
     }
 }
