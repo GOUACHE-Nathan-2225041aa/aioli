88b96f951a1e978c3b0930b1783cbaa1c90bc023
nathanGouacheg22017449 <nathan.gouache@etu.univ-amu.fr>
2024-04-03 13:15:04 +0200

    Ajout prix et description

--- src/main/java/fr/univamu/iut/plats/Put.java
+++ src/main/java/fr/univamu/iut/plats/Put.java
@@ -12,9 +12,9 @@ import java.sql.SQLException;
 public class Put {
     @PUT
     @Consumes("application/x-www-form-urlencoded")
-    public boolean putPlat(@FormParam("nameToReplace")String name, @FormParam("id")String id){
+    public boolean putPlat(@FormParam("nom") String nom, @FormParam("prix") String prix, @FormParam("description") String description, @FormParam("id")String id){
         try{
-            SqlRequests.executeQuery("UPDATE Plats SET nom='"+name+"' WHERE id='"+id+"'");
+            SqlRequests.executeQuery("UPDATE Plats SET nom='"+nom+"', prix='"+prix+"',description='"+description+"', WHERE id='"+id+"'");
         }
         catch(SQLException | ClassNotFoundException e){
             return false;
