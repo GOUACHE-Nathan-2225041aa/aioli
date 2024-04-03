7a44353128e8f047703e451f50fddcb987accc57
nathanGouacheg22017449 <nathan.gouache@etu.univ-amu.fr>
2024-04-03 13:26:14 +0200

    Fix prix

--- src/main/java/fr/univamu/iut/plats/Post.java
+++ src/main/java/fr/univamu/iut/plats/Post.java
@@ -9,10 +9,10 @@ public class Post {
 
     @POST
     @Consumes("application/x-www-form-urlencoded")
-    public boolean createPlat(@FormParam("nom") String nom, @FormParam("price") String prix, @FormParam("description") String description){
+    public boolean createPlat(@FormParam("nom") String nom, @FormParam("prix") float prix, @FormParam("description") String description){
 
         try{
-            SqlRequests.executeQuery("INSERT INTO Plats (nom, prix, description) VALUES ('"+nom+"', '"+prix+"','"+description+"')");
+            SqlRequests.executeQuery("INSERT INTO Plats (nom, prix, description) VALUES ('"+nom+"', "+prix+",'"+description+"')");
         }
         catch(SQLException | ClassNotFoundException e){
             return false;
