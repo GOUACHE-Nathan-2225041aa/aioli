bb779a09db49ae2129d1d5793741e8034d8d5cfd
nathanGouacheg22017449 <nathan.gouache@etu.univ-amu.fr>
2024-04-04 17:02:18 +0200

    Avec la javadoc

--- src/main/java/fr/univamu/iut/Menus.java
+++ src/main/java/fr/univamu/iut/Menus.java
@@ -14,41 +14,65 @@ import java.util.ArrayList;
 
 @Path("/menus")
 public class Menus {
+
+    /**
+     * Crée un nouveau menu avec les plats spécifiés pour un utilisateur donné.
+     *
+     * @param nom_user Le nom de l'utilisateur.
+     * @param plats    Les identifiants des plats à inclure dans le menu (séparés par des virgules).
+     * @return true si le menu a été créé avec succès, false sinon.
+     */
     @POST
     @Consumes("application/x-www-form-urlencoded")
-    public boolean create(@FormParam("nom_user") String nom_user, @FormParam("plats") String plats){
+    public boolean create(@FormParam("nom_user") String nom_user, @FormParam("plats") String plats) {
 
+        // Supprimer les crochets de la chaîne plats
         plats = plats.replace("[", "").replace("]", "");
+
+        // Diviser les identifiants des plats en une liste
         String[] identifiants = plats.split(",");
 
+        // Liste pour stocker les réponses des requêtes pour chaque plat
         ArrayList<JSONObject> reponses = new ArrayList<JSONObject>();
 
-        for(String i : identifiants){
+        // Parcourir les identifiants et récupérer les détails de chaque plat
+        for (String i : identifiants) {
             Client client = ClientBuilder.newClient();
-            String response = client.target( "http://localhost:8080/Plats_et_users-1.0-SNAPSHOT/api/plats/"+i ).request(MediaType.TEXT_PLAIN_TYPE).get(String.class);
+            String response = client.target("http://localhost:8080/Plats_et_users-1.0-SNAPSHOT/api/plats/" + i)
+                    .request(MediaType.TEXT_PLAIN_TYPE).get(String.class);
 
             reponses.add(new JSONObject(response));
         }
 
+        // Calculer le prix total des plats sélectionnés
         double prixFinal = 0;
 
+        // Obtenir la date actuelle au format SQL
         java.util.Date date = new java.util.Date();
-
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         String sqlDate = sdf.format(date);
 
-        for(JSONObject a : reponses){
-            prixFinal+=a.getDouble("prix");
+        // Ajouter le prix de chaque plat à prixFinal
+        for (JSONObject a : reponses) {
+            prixFinal += a.getDouble("prix");
         }
-        try{
-            SqlRequests.executeQuery("INSERT INTO Menus (nom_user, date_creation, plats, prix) VALUES ('"+nom_user+"', '"+sqlDate+"', '"+ plats +"', "+prixFinal+")");
-        }
-        catch(SQLException | ClassNotFoundException e){
+
+        // Insérer le nouveau menu dans la base de données
+        try {
+            SqlRequests.executeQuery("INSERT INTO Menus (nom_user, date_creation, plats, prix) VALUES ('" + nom_user + "', '" + sqlDate + "', '" + plats + "', " + prixFinal + ")");
+        } catch (SQLException | ClassNotFoundException e) {
             return false;
         }
         return true;
     }
 
+    /**
+     * Récupère tous les menus disponibles.
+     *
+     * @return Une chaîne JSON représentant tous les menus.
+     * @throws SQLException          Si une erreur survient lors de l'exécution de la requête SQL.
+     * @throws ClassNotFoundException Si la classe de la base de données n'est pas trouvée.
+     */
     @GET
     @Produces("text/plain")
     public String get() throws SQLException, ClassNotFoundException {
@@ -77,6 +101,14 @@ public class Menus {
         return result.toString().replace("[", "{").replace("]", "}");
     }
 
+    /**
+     * Récupère un menu spécifique en fonction de son identifiant.
+     *
+     * @param id L'identifiant du menu à récupérer.
+     * @return Une chaîne JSON représentant le menu trouvé.
+     * @throws SQLException          Si une erreur survient lors de l'exécution de la requête SQL.
+     * @throws ClassNotFoundException Si la classe de la base de données n'est pas trouvée.
+     */
     @GET
     @Produces("text/plain")
     @Path("/{id}")
@@ -105,13 +137,18 @@ public class Menus {
         return result.toString().replace("[", "{").replace("]", "}");
     }
 
+    /**
+     * Supprime un menu existant en fonction de son identifiant.
+     *
+     * @param id L'identifiant du menu à supprimer.
+     * @return true si le menu a été supprimé avec succès, false sinon.
+     */
     @DELETE
     @Path("/{id}")
-    public boolean deletePlatWithId(@PathParam("id")String id){
-        try{
-            SqlRequests.executeQuery("DELETE FROM Menus WHERE id='"+id+"'");
-        }
-        catch(SQLException | ClassNotFoundException e){
+    public boolean deletePlatWithId(@PathParam("id") String id) {
+        try {
+            SqlRequests.executeQuery("DELETE FROM Menus WHERE id='" + id + "'");
+        } catch (SQLException | ClassNotFoundException e) {
             return false;
         }
         return true;
--- src/main/java/fr/univamu/iut/Plats.java
+++ src/main/java/fr/univamu/iut/Plats.java
@@ -6,20 +6,40 @@ import org.json.JSONObject;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
+/**
+ * Cette classe gère les opérations CRUD (Create, Read, Update, Delete) pour les plats.
+ * Les opérations sont exposées en tant que services web RESTful.
+ */
 @Path("/plats")
 public class Plats {
+
+    /**
+     * Crée un nouveau plat avec les informations fournies.
+     *
+     * @param nom         Le nom du plat.
+     * @param prix        Le prix du plat.
+     * @param description La description du plat.
+     * @return true si le plat a été créé avec succès, false sinon.
+     */
     @POST
     @Consumes("application/x-www-form-urlencoded")
-    public boolean create(@FormParam("nom") String nom, @FormParam("prix") float prix, @FormParam("description") String description){
-
-        try{
-            SqlRequests.executeQuery("INSERT INTO Plats (nom, prix, description) VALUES ('"+nom+"', "+prix+",'"+description+"')");
-        }
-        catch(SQLException | ClassNotFoundException e){
+    public boolean create(@FormParam("nom") String nom, @FormParam("prix") float prix, @FormParam("description") String description) {
+        try {
+            SqlRequests.executeQuery("INSERT INTO Plats (nom, prix, description) VALUES ('" + nom + "', " + prix + ",'" + description + "')");
+        } catch (SQLException | ClassNotFoundException e) {
             return false;
         }
         return true;
     }
+
+    /**
+     * Récupère tous les plats disponibles.
+     *
+     * @return Une chaîne JSON représentant tous les plats.
+     * @throws SQLException          Si une erreur survient lors de l'exécution de la requête SQL.
+     * @throws ClassNotFoundException Si la classe de la base de données n'est pas trouvée.
+     */
     @GET
     @Produces("text/plain")
     public String get() throws SQLException, ClassNotFoundException {
@@ -46,6 +66,14 @@ public class Plats {
         return result.toString();
     }
 
+    /**
+     * Récupère un plat spécifique en fonction de son identifiant.
+     *
+     * @param id L'identifiant du plat à récupérer.
+     * @return Une chaîne JSON représentant le plat trouvé.
+     * @throws SQLException          Si une erreur survient lors de l'exécution de la requête SQL.
+     * @throws ClassNotFoundException Si la classe de la base de données n'est pas trouvée.
+     */
     @GET
     @Produces("text/plain")
     @Path("/{id}")
@@ -72,26 +100,38 @@ public class Plats {
         return result.toString().replace("[", "").replace("]", "");
     }
 
+    /**
+     * Met à jour les informations d'un plat existant.
+     *
+     * @param nom         Le nouveau nom du plat.
+     * @param prix        Le nouveau prix du plat.
+     * @param description La nouvelle description du plat.
+     * @param id          L'identifiant du plat à mettre à jour.
+     * @return true si le plat a été mis à jour avec succès, false sinon.
+     */
     @PUT
     @Consumes("application/x-www-form-urlencoded")
-    public boolean put(@FormParam("nom") String nom, @FormParam("prix") float prix, @FormParam("description") String description, @FormParam("id")int id){
-        try{
-            SqlRequests.executeQuery("UPDATE Plats SET nom='"+nom+"', prix='"+prix+"',description='"+description+"' WHERE id='"+id+"'");
-        }
-        catch(SQLException | ClassNotFoundException e){
+    public boolean put(@FormParam("nom") String nom, @FormParam("prix") float prix, @FormParam("description") String description, @FormParam("id") int id) {
+        try {
+            SqlRequests.executeQuery("UPDATE Plats SET nom='" + nom + "', prix='" + prix + "',description='" + description + "' WHERE id='" + id + "'");
+        } catch (SQLException | ClassNotFoundException e) {
             return false;
         }
         return true;
     }
 
-
+    /**
+     * Supprime un plat existant en fonction de son identifiant.
+     *
+     * @param id L'identifiant du plat à supprimer.
+     * @return true si le plat a été supprimé avec succès, false sinon.
+     */
     @DELETE
     @Path("/{id}")
-    public boolean delete(@PathParam("id")int id){
-        try{
-            SqlRequests.executeQuery("DELETE FROM Plats WHERE id='"+id+"'");
-        }
-        catch(SQLException | ClassNotFoundException e){
+    public boolean delete(@PathParam("id") int id) {
+        try {
+            SqlRequests.executeQuery("DELETE FROM Plats WHERE id='" + id + "'");
+        } catch (SQLException | ClassNotFoundException e) {
             return false;
         }
         return true;
